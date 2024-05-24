e5d002233ba9a8070613e91e7482006de0554dd8
Serhiy Tymoshenko <serhiy.tymoshenko@priority-software.com>
2024-05-24 11:16:40 +0300

    toBeHiden example

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,18 +1,33 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test.describe('toBeHidden() timeout should has higher priority then "strict mode violation" rule', () => {
+    test('toBeHidden() timeout should not be ignored', async ({ page }) => {
+        await page.goto('https://playwright.dev/');
+        await page.getByLabel('Search').click();
+        await page.locator('#docsearch-input').fill(`test`);
+        await page.waitForTimeout(2000); // wait for the search results to appear
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
+        const timeout = 10000;
+        const start = Date.now();
+        await expect(page.locator('[role="option"]'))
+            .toBeHidden({ timeout: timeout })
+            .catch(e => console.log(e));
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+        //now test "toBeHidden" actual timeout
+        expect(Date.now() - start).toBeGreaterThanOrEqual(timeout);
+    });
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+    test('list elements expected to be hidden should be awaited to hide', async ({ page }) => {
+        await test.step('#1 - filter the list by relevant search key', async () => {
+            await page.goto('https://playwright.dev/');
+            await page.getByLabel('Search').click();
+            await page.locator('#docsearch-input').fill(`test`);
+            await expect(page.locator('[role="option"]').nth(0)).toBeVisible();
+        });
+        await test.step('#2 - clear the search key and type some irrelevant one for list elements to be hidden', async () => {
+            await page.locator('#docsearch-input').fill('you should never find me');
 
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+            await expect(page.locator('[role="option"]')).toBeHidden({ timeout: 10000 });
+        });
+    });
 });
